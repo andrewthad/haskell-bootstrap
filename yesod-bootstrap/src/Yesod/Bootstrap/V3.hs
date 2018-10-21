fc935f6d6e716a45dc0104e2323628323603d1a4
Andrew Martin <andrew.thaddeus@gmail.com>
2018-10-21 11:30:13 -0400

    reintroduce glyphiconFeedback

--- yesod-bootstrap/src/Yesod/Bootstrap/V3.hs
+++ yesod-bootstrap/src/Yesod/Bootstrap/V3.hs
@@ -47,6 +47,9 @@ formButton method ctx size route inner = do
 glyphicon :: Monad m => Text -> WidgetT site m ()
 glyphicon s = span_ [class_ $ toValue $ "glyphicon glyphicon-" <> s] mempty
 
+glyphiconFeedback :: Text -> WidgetT site IO ()
+glyphiconFeedback s = span_ [class_ (toValue (Text.concat ["glyphicon glyphicon-", s, " form-control-feedback"]))] mempty
+
 label :: B.Context -> WidgetT site IO () -> WidgetT site IO ()
 label ctx = span_ [class_ $ toValue $ "label label-" <> B.contextClass ctx]
 
